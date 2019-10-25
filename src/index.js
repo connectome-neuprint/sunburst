df67ff973e0111508420476e8fa6a20bf5579ad4
Jody Clements <jody@neomorphic.co.uk>
2019-10-25 15:57:26 -0400

    Adds babel transpiler for building

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import * as d3 from 'd3';
+
 const DEFAULT_PRIMARY_COLOR = "#ff0000";
 const DEFAULT_SECONDARY_COLOR = "#0000ff";
+
 
 export default class sunburst {
   constructor(props = {}) {
@@ -15,6 +18,9 @@ export default class sunburst {
 
   render(target) {
     // use d3 to append an svg element to the 'target' container.
+    const { data } = this;
+    const color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1))
+    console.log(color);
     console.log('rendering to', target);
   }
 }
