f58975bd1f27ba7f31569820998011e848e41581
Jody Clements <jody@neomorphic.co.uk>
2019-10-25 15:17:35 -0400

    Adds code linting and formatting.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-const DEFAULT_PRIMARY_COLOR = '#ff0000';
-const DEFAULT_SECONDARY_COLOR = '#0000ff';
+const DEFAULT_PRIMARY_COLOR = "#ff0000";
+const DEFAULT_SECONDARY_COLOR = "#0000ff";
 
 export default class sunburst {
-  constructor(props={}) {
+  constructor(props = {}) {
     this.data = props.data || {};
     this.primaryColor = props.primaryColor || DEFAULT_PRIMARY_COLOR;
     this.secondaryColor = props.secondaryColor || DEFAULT_SECONDARY_COLOR;
   }
 
+  setData(dataObject) {
+    this.data = dataObject;
+    return this;
+  }
+
   render(target) {
     // use d3 to append an svg element to the 'target' container.
-    //
+    console.log('rendering to', target);
   }
 }
